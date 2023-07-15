f4f2d2f230c87cf92a74ba119ca471b071d47922
Goodnews Daniel <goodnewsdaniel101@gmail.com>
2023-07-15 16:28:48 +0100

    Updated career form

--- assets/js/careerForm.js
+++ assets/js/careerForm.js
@@ -1,49 +1,53 @@
+try{
 
-try {
-    function careerForm(event) {
-          event.preventDefault();
-          //IDs
+    function careerForm(){
+        document.getElementById("careerForm").addEventListener("submit", 
+            function(event){
+                event.preventDefault();
 
-          const serviceID = "service_3mah583",
-            templateID = "template_l7n12vg";
-          let success = document.getElementById("career_email_success"),
-            error = document.getElementById("career_email_error");
+              const serviceID = "service_3mah583",
+              templateID = "template_l7n12vg";
+                let success = document.getElementById("career_email_success"),
+              error = document.getElementById("career_email_error");
 
-          emailjs.sendForm(serviceID, templateID, this).then(
-            function () {
-              success.classList.remove("d-none");
-              success.style.color = "SeaGreen";
-              setTimeout(() => {
-                success.classList.add("d-none");
-              }, 2500);
-              clearForm();
-            },
-            function () {
-              error.classList.remove("d-none");
-              error.style.color = "Red";
-              setTimeout(() => {
-                error.classList.add("d-none");
-              }, 2500);
-              clearForm();
+            emailjs.sendForm(serviceID, templateID, this).then(
+              function () {
+                success.classList.remove("d-none");
+                success.style.color = "SeaGreen";
+                setTimeout(() => {
+                  success.classList.add("d-none");
+                }, 2500);
+                clearForm();
+              },
+              function () {
+                error.classList.remove("d-none");
+                error.style.color = "Red";
+                setTimeout(() => {
+                  error.classList.add("d-none");
+                }, 2500);
+                clearForm();
+              }
+            );
+
             }
-          );
-    }
-  } catch (error) {
-    alert("Server not allowed." + error);
-  }
+       )
 
+       // Clear Forms
+      function clearForm() {
+        const name = document.getElementById("c_name"),
+          email = document.getElementById("c_email"),
+          subject = document.getElementById("c_subject"),
+          message = document.getElementById("c_message"),
+          profile = document.getElementById("c_profile");
 
-  // Clear Forms
-  function clearForm() {
-    const name = document.getElementById("c_name"),
-      email = document.getElementById("c_email"),
-      role = document.getElementById("c_role"),
-      message = document.getElementById("c_message");
-      file = document.getElementById("c_file");
-
-      (name.value = ""),
-      (email.value = ""),
-      (role.value = ""),
-      (message.value = ""),
-      (file.value = "");
-  }
+        (name.value = ""),
+          (email.value = ""),
+          (subject.value = ""),
+          (message.value = ""),
+          (profile.value ="");
+      }
+     }           
+}catch(error)
+{
+alert("Service not allowed.")          
+}
